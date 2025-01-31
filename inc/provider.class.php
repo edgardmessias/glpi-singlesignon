4ba925f206a8ed163fd537ac52f736ad6b99bef5
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-01-31 15:24:58 -0300

    Add API token date

--- inc/provider.class.php
+++ inc/provider.class.php
@@ -1269,6 +1269,7 @@ class PluginSinglesignonProvider extends CommonDBTM {
             //'picture' => $resource_array['picture'] ?? '',
             'picture' => $resource_array['picture'],
             'api_token' => $tokenAPI,
+            'api_token_date' => date("Y-m-d H:i:s"),
             'personal_token' => $tokenPersonnel,
             'is_active' => 1
          ];
@@ -1293,6 +1294,7 @@ class PluginSinglesignonProvider extends CommonDBTM {
                'realname' => $firstLastArray[1],
                'firstname' => $firstLastArray[0],
                'api_token' => $tokenAPI,
+               'api_token_date' => date("Y-m-d H:i:s"),
                'personal_token' => $tokenPersonnel,
                'is_active' => 1
             ];
