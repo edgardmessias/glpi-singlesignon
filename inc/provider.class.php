6997c61954e8e7f62a6eacae0fb7787bbf276a38
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-10-15 10:02:11 +0200

    Check if Provider class exists before requiring the file

--- inc/provider.class.php
+++ inc/provider.class.php
@@ -1,4 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../src/Provider.php';
+if (!class_exists(\Glpi\Plugin\Singlesignon\Provider::class, false)) {
+   require_once __DIR__ . '/../src/Provider.php';
+}
 
