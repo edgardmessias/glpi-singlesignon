4594eceddb50d8a6171349a6eb0ffa8f6cd5aca8
Brandon Gachenot <brandon@gachenot.eu>
2026-01-14 12:28:51 +0100

    fix: multiple critical errors
    feat: openid groups support
    feat: secure curl options by default
    feat: improved user first_name and last_name detection
    fix: firewall picture.send.php

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@ use Glpi\Plugin\Hooks;
 use GlpiPlugin\Singlesignon\LoginRenderer;
 use GlpiPlugin\Singlesignon\Preference;
 use GlpiPlugin\Singlesignon\Provider;
-define('PLUGIN_SINGLESIGNON_VERSION', '1.5.1');
+define('PLUGIN_SINGLESIGNON_VERSION', '1.6.0');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_SINGLESIGNON_MIN_GLPI', '11.0.0');
@@ -76,6 +76,9 @@ function plugin_init_singlesignon() {
    $PLUGIN_HOOKS['menu_toadd']['singlesignon'] = [
       'config'  => Provider::class,
    ];
+
+   // Make the picture.send.php page public again.
+   Firewall::addPluginStrategyForLegacyScripts('singlesignon', '#^/front/picture.send.php$#', Firewall::STRATEGY_NO_CHECK);
 }
 
 // Get the name and the version of the plugin - Needed
