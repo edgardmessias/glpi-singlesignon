d96123933eea85720bc014b99533ba6471a3c6f7
E <2974895+eduardomozart@users.noreply.github.com>
2026-05-25 00:20:33 -0300

    Update ToolboxPlugin.php

--- src/ToolboxPlugin.php
+++ src/ToolboxPlugin.php
@@ -403,4 +403,36 @@ class ToolboxPlugin
 
         return array_values(array_unique($filtered));
     }
+
+    /**
+     * Log a plugin failure to the singlesignon log file.
+     *
+     * @param string $function     Name of the function where the failure occurred (use __FUNCTION__).
+     * @param string $message      Human-readable failure description.
+     * @param string $providerName Name of the SSO provider, if available.
+     * @param int    $providerId   Database ID of the provider, if available.
+     * @param string $userName     Login name of the GLPI user, if available.
+     * @param int    $userId       Database ID of the user, if available.
+     */
+    public static function logFailure(
+        string $function,
+        string $message,
+        string $providerName = '',
+        int $providerId = 0,
+        string $userName = '',
+        int $userId = 0
+    ): void {
+        Toolbox::logInFile(
+            'singlesignon-errors',
+            sprintf(
+                "[%s] provider=\"%s\" provider_id=%d user=\"%s\" user_id=%d %s\n",
+                $function,
+                $providerName,
+                $providerId,
+                $userName,
+                $userId,
+                $message,
+            ),
+        );
+    }
 }
