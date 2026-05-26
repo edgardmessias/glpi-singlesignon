b8b55d8248bcec45b47604709182b0d881afe5f0
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2026-05-26 09:36:21 -0300

    Remove unused fields from Provider class

--- src/Provider.php
+++ src/Provider.php
@@ -174,9 +174,7 @@ class Provider extends CommonDBTM
         $this->fields['auto_register'] = 0;
         $this->fields['registration_preview'] = 0;
         $this->fields['default_entities_id'] = 0;
-        $this->fields['default_entities_id_is_recursive'] = 0;
         $this->fields['default_profiles_id'] = 0;
-        $this->fields['default_profiles_id_is_recursive'] = 0;
         $this->fields['ssl_verify_host'] = 1;
         $this->fields['ssl_verify_peer'] = 1;
     }
